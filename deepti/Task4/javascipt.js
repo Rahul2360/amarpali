4e9ece9d961a6c54514472ea221b52db5f7e9ac0
DRajpoot2 <deeptirajpoot23@gmail.com>
2022-01-11 10:25:33 +0530

    few changes

---
+++ deepti/Task4/javascipt.js
@@ -0,0 +1,8 @@
+function myfunction() {
+    var pwd = document.getElementById("one").value;
+    var pwd2 = document.getElementById("two").value;
+    if (pwd != pwd2) {
+        alert("Wrong password");
+    }
+
+}
