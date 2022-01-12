052b316f5631f13b939ecc7abfe7f01168384c7a
DRajpoot2 <deeptirajpoot23@gmail.com>
2022-01-13 03:37:31 +0530

    modified

--- deepti/11jantask1(chess)/chboard.js
+++ deepti/11jantask1(chess)/chboard.js
@@ -1,22 +1,27 @@
- var center = document.createElement('center');
- var ChessTable = document.createElement('table');
- 
- for (var i = 0; i < 8; i++) {
-     var tr = document.createElement('tr');
-     for (var j = 0; j < 8; j++) {
-         var td = document.createElement('td');
-         if ((i + j) % 2 == 0) {
-             td.setAttribute('class', 'cell white');
-             tr.appendChild(td);
+function chessboard() {
+    const white_piece = ['&#9814', '&#9816', '&#9815', '&#9813', '&#9812', '&#9815', '&#9816', '&#9814'];
+    const W_Pawn = '&#9817';
+    const black_piece = ['&#9820', '&#9822', '&#9821', '&#9813', '&#9819', '&#9821', '&#9822', '&#9820'];
+    const B_Pawn = '&#9823';
+    var chessboard = document.getElementById("chessboard");
 
-         } else {
-             td.setAttribute('class', 'cell black');
-             tr.appendChild(td);
-         }
-     }
-     ChessTable.appendChild(tr);
- }
- center.appendChild(ChessTable);
- ChessTable.setAttribute('cellspacing', '0');
- ChessTable.setAttribute('width', '600px');
- document.body.appendChild(center);
+    for (var i = 0; i < 8; i++) {
+        var tr = chessboard.appendChild(document.createElement("div"));
+        for (var j = 0; j < 8; j++) {
+            var span = document.createElement("span");
+            var color = ((j + i) % 2 == 0) ? "#cecece" : "#fff";
+            span.style.cssText = `background: ${color}`;
+            if (i == 0) {
+                span.innerHTML += '<span class="piece" id="' + i + j + '">' + white_piece[j] + '</span>';
+            } else if (i == 1) {
+                span.innerHTML += '<span class="piece" id="' + i + j + '">' + W_Pawn + '</span>';
+            } else if (i == 6) {
+                span.innerHTML += B_Pawn;
+            } else if (i == 7) {
+                span.innerHTML += black_piece[j];
+            } else {}
+            tr.appendChild(span);
+        }
+    }
+}
+chessboard()
--- deepti/11jantask3(form2)/surv.js
+++ deepti/11jantask3(form2)/surv.js
@@ -1,3 +1,19 @@
+function selectBtn() {
+    var sel = document.getElementById('DIVbtns').getElementsByTagName('div');
+    for (var i = 0; i < sel.length; i++) {
+        sel[i].style.backgroundColor = "yellow";
+    }
+    document.getElementById().style.backgroundColor = 'aqua';
+}
+console.log(sel);
+
+function mandatory() {
+    var a = document.getElementsByName("compname")[0].value;
+    if (a == "") {
+        alert("Company name must be filled out");
+    }
+}
+
 function validate() {
     var e = document.getElementsByName("email")[0].value;
     var atindex = e.indexOf('@');
@@ -6,3 +22,11 @@ function validate() {
         alert("Wrong email");
     }
 }
+
+var form = document.getElementsById('form')
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault() //prevents autosubmitting
+    var scale = document.getElementsByClassName('chart-scale').value
+    console.log(scale)
+})
