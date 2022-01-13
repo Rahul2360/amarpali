0a9649b0b0f0be6016d8aeff5bb1ec12eaed700e
DRajpoot2 <deeptirajpoot23@gmail.com>
2022-01-13 14:29:05 +0530

    improvement

--- deepti/11jantask3(form2)/surv.js
+++ deepti/11jantask3(form2)/surv.js
@@ -18,10 +18,45 @@ function validate() {
     var e = document.getElementsByName("email")[0].value;
     var atindex = e.indexOf('@');
     var dotindex = e.lastIndexOf('.');
-    if (atindex < 1 || dotindex >= e.lenght - 2 || dotindex - atindex < 3) {
+    if (atindex < 1 || dotindex >= e.length - 2 || dotindex - atindex < 3) {
         alert("Wrong email");
     }
 }
+
+var a;
+
+function show_hide() {
+    if (a == 1) {
+        document.getElementById("name").style.display = "inline";
+        document.getElementById("company").style.display = "inline";
+        document.getElementById("state").style.display = "inline";
+        document.getElementById("city").style.display = "inline";
+        document.getElementById("number").style.display = "inline";
+        document.getElementById("email").style.display = "inline";
+        document.getElementById("big_text").style.display = "inline";
+        document.getElementById("quality").style.display = "inline";
+        document.getElementById("trainer").style.display = "inline";
+        document.getElementById("manner").style.display = "inline";
+        document.getElementById("experience").style.display = "inline";
+        document.getElementById("rating").style.display = "inline";
+        return a = 0;
+    } else {
+        document.getElementById("name").style.display = "none";
+        document.getElementById("company").style.display = "none";
+        document.getElementById("state").style.display = "none";
+        document.getElementById("city").style.display = "none";
+        document.getElementById("number").style.display = "none";
+        document.getElementById("email").style.display = "none";
+        document.getElementById("big_text").style.display = "none";
+        document.getElementById("quality").style.display = "none";
+        document.getElementById("trainer").style.display = "none";
+        document.getElementById("manner").style.display = "none";
+        document.getElementById("experience").style.display = "none";
+        document.getElementById("rating").style.display = "none";
+        return a = 1;
+    }
+}
+
 
 var form = document.getElementsById('form')
 
@@ -29,4 +64,6 @@ form.addEventListener('submit', function(event) {
     event.preventDefault() //prevents autosubmitting
     var scale = document.getElementsByClassName('chart-scale').value
     console.log(scale)
+    var expe = document.getElementById("result").value
+    console.log(expe)
 })
