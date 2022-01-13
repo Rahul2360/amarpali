cbf9f5517b52fa9956f7608af47ab6b2375b8a59
Devendra <dnegi0201@gmail.com>
2022-01-13 15:47:30 +0530

    print in console

--- devendra/survey form/servey.js
+++ devendra/survey form/servey.js
@@ -1,35 +1,59 @@
 var a;
+var naam = document.getElementById("name");
+var company = document.getElementById("company");
+var state = document.getElementById("state");
+var city = document.getElementById("city");
+var number =  document.getElementById("number");
+var email = document.getElementById("email");
+var big_text = document.getElementById("big_text");
+var quality = document.getElementById("quality");
+var trainer = document.getElementById("trainer");
+var manner = document.getElementById("manner");
+var experience = document.getElementById("experience");
+var rating = document.getElementById("rating");
 function show_hide()
 {
     if(a==1)
     {
-        document.getElementById("name").style.display="inline";
-        document.getElementById("company").style.display="inline";
-        document.getElementById("state").style.display="inline";
-        document.getElementById("city").style.display="inline";
-        document.getElementById("number").style.display="inline";
-        document.getElementById("email").style.display="inline";
-        document.getElementById("big_text").style.display="inline";
-        document.getElementById("quality").style.display="inline";
-        document.getElementById("trainer").style.display="inline";
-        document.getElementById("manner").style.display="inline";
-        document.getElementById("experience").style.display="inline";
-        document.getElementById("rating").style.display="inline";
+        naam.style.display="inline";
+        company.style.display="inline";
+        state.style.display="inline";
+        city.style.display="inline";
+       number.style.display="inline";
+        email.style.display="inline";
+        big_text.style.display="inline";
+        quality.style.display="inline";
+       trainer.style.display="inline";
+        manner.style.display="inline";
+       experience.style.display="inline";
+        rating.style.display="inline";
         return a=0;
     }
     else{
-        document.getElementById("name").style.display="none";
-        document.getElementById("company").style.display="none";
-        document.getElementById("state").style.display="none";
-        document.getElementById("city").style.display="none";
-        document.getElementById("number").style.display="none";
-        document.getElementById("email").style.display="none";
-        document.getElementById("big_text").style.display="none";
-        document.getElementById("quality").style.display="none";
-        document.getElementById("trainer").style.display="none";
-        document.getElementById("manner").style.display="none";
-        document.getElementById("experience").style.display="none";
-        document.getElementById("rating").style.display="none";
+        naam.style.display="none";
+        company.style.display="none";
+        state.style.display="none";
+        city.style.display="none";
+        number.style.display="none";
+        email.style.display="none";
+        big_text.style.display="none";
+        quality.style.display="none";
+        trainer.style.display="none";
+        manner.style.display="none";
+        experience.style.display="none";
+        rating.style.display="none";
         return a=1;
     }
+}
+
+function display_console(){
+   
+    console.log(naam.value);
+    console.log(company.value);
+    console.log(state.value);
+    console.log(city.value);
+    console.log(number.value);
+    console.log(email.value);
+    console.log(big_text.value);
+    console.log(rating.value);
 }
