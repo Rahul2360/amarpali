ef8789bd2b14b569b4cf24f9749e9af737253428
DRajpoot2 <deeptirajpoot23@gmail.com>
2022-02-15 18:47:26 +0530

    search by policy number in excel

--- deepti/python/15feb/excelsearch.py
+++ deepti/python/15feb/excelsearch.py
@@ -6,25 +6,24 @@ total_sheet=format(workbook.nsheets)
 total_sheet=int(total_sheet)
 policy_no=input("Enter Policy Number ")
 sheetnames=workbook.sheet_names()
-current=[]
-my_sheet_dict={}
 workbook_d = Workbook()
 
-for sheet_no in range(0,total_sheet):
+for sheet_no in range(total_sheet):
     select_sheet=workbook.sheet_by_index(sheet_no)
     worksheet_d= workbook_d.create_sheet(sheetnames[sheet_no])
-    my_sheet_list=[]
     maximum_col=select_sheet.ncols
     maximum_row=select_sheet.nrows
   
-    for j in range(0,maximum_row):
-        value=select_sheet.cell_value(j,0)
-        all_heading_dict={}
+    for j in range(maximum_row):
         all_data_list=[]
+        if j==0:
+            for p in range(maximum_col):
+                all_data_list.append(select_sheet.cell_value(j,p))
+        value=select_sheet.cell_value(j,0)
 
         if value==policy_no:   
-            for k in range(0,maximum_col):
+            for k in range(maximum_col):
                 all_data_list.append(select_sheet.cell_value(j,k))  
-            worksheet_d.append(all_data_list)
-                
+        worksheet_d.append(all_data_list)
+          
 workbook_d.save("/home/deepti/Downloads/combinedexcel.xlsx")     
